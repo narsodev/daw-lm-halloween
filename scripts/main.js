e624804c94447c3738595b94d00967415b0ee092
narsodev <narsodev@gmail.com>
2021-11-14 13:06:50 +0100

    Add speech synthesis and hall scene

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,42 @@
 const $ = document.querySelector.bind(document)
 
+const synth = window.speechSynthesis
+
+const speak = (
+  {
+    text,
+    config = {
+      voice: 'Google UK English Male',
+      pitch: 0.4,
+      rate: 0.8
+    },
+    onStart = () => {},
+    onEnd = () => {}
+  }
+) => {
+  if (synth.speaking) {
+    console.error('speechSynthesis.speaking')
+    return
+  }
+  const utterThis = new SpeechSynthesisUtterance(text)
+  utterThis.volume = 1
+  utterThis.onerror = () => {
+    alert('Error speaking')
+    console.error('Error speaking')
+  }
+  utterThis.onstart = onStart
+  utterThis.onend = onEnd
+
+  const { voice, pitch, rate } = config
+
+  utterThis.voice = synth.getVoices().find(voiceObject => voiceObject.name === voice)
+  utterThis.pitch = pitch
+  utterThis.rate = rate
+
+  window.speechSynthesis.cancel()
+  synth.speak(utterThis)
+}
+
 let playername
 
 const backgroundAudio = $('#background-audio')
@@ -13,13 +50,13 @@ const makeJumpscare = (img, audio) => {
 
   audio.addEventListener('ended', () => {
     img.classList.toggle('hidden')
-    console.log('ended')
   })
 }
 
 const scenes = {
-  menu: $('.menu'),
-  house: $('.house')
+  menu: $('.scene.menu'),
+  house: $('.scene.house'),
+  hall: $('.scene.hall')
 }
 
 const nameInput = $('#name')
@@ -35,14 +72,42 @@ form.addEventListener('submit', e => {
   scenes.house.classList.toggle('hidden')
 })
 
-const sceneHouseObserver = new MutationObserver(mutations => {
+const sceneHouseObserver = new MutationObserver(() => {
   sceneHouseObserver.disconnect()
+
+  backgroundAudio.volume = 0.5
   backgroundAudio.play()
+
+  speak({
+    text: `Hi ${playername}.
+    I am trapped in this house.
+    Can you go in and help me escape?
+    I need to find a way out.
+    Try no to look through the windows.
+    `,
+    onStart: () => {
+      backgroundAudio.volume = 0.5
+    },
+    onEnd: () => {
+      backgroundAudio.volume = 1
+    }
+  })
 
   const jumpscare = $('#jumpscare-1')
   const [activator, audio, img] = jumpscare.children
   activator.addEventListener('mouseover', () => {
     makeJumpscare(img, audio)
+  }, ({ once: true }))
+
+  const door = $('#door')
+  door.addEventListener('click', () => {
+    scenes.house.classList.toggle('hidden')
+    scenes.hall.classList.toggle('hidden')
   })
 })
 sceneHouseObserver.observe(scenes.house, { attributes: true })
+
+const sceneHallObserver = new MutationObserver(() => {
+  sceneHallObserver.disconnect()
+})
+sceneHallObserver.observe(scenes.hall, { attributes: true })
